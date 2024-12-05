168bc0b9ff956902ce1395146485e5458059698a
Douglas Sanchez <38049028+dougdesigner@users.noreply.github.com>
2024-12-04 23:40:15 -0800

    Reviewer feedback to remove redundancy

--- src/components/generic/EmptyState.tsx
+++ src/components/generic/EmptyState.tsx
@@ -14,16 +14,18 @@ const EmptyState: React.FC<EmptyStateProps> = ({ title, description, buttonText,
             <h3 className="mt-2 text-sm font-semibold text-slate-400">{title}</h3>
             <p className="mt-1 text-sm text-slate-500">{description}</p>
             <div className="mt-6">
-                <button
-                    type="button"
-                    onClick={onButtonClick}
-                    className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                    <svg className="-ml-0.5 mr-1.5 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                        <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
-                    </svg>
-                    {buttonText}
-                </button>
+                <div className="hidden">
+                    <button
+                        type="button"
+                        onClick={onButtonClick}
+                        className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        <svg className="-ml-0.5 mr-1.5 size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                            <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+                        </svg>
+                        {buttonText}
+                    </button>
+                </div>
             </div>
         </div>
     );
